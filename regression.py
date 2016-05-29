880ed496f5a1a538465210488997b2faedafc4df
warden3344 <592558773@qq.com>
2016-05-29 14:55:39 +0800

    add the regression test

---
+++ regression.py
@@ -0,0 +1,30 @@
+from numpy import *
+def loadDataSet(filename):
+    numFeat = len(open(filename).readline().split('\t')) -1
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+def standRegres(xArr,yArr):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    xTx = xMat.T*xMat
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I * (xMat.T*yMat)
+    return ws
+
+xArr,yArr = loadDataSet('ex0.txt')
+ws = standRegres(xArr, yArr)
+
+print ws
+print xArr
+print yArr
